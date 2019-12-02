2e0cc91eeef87a9ba1c276c473e0526944e44d43
Hang Zhou <zhou_hang@berkeley.edu>
2019-12-02 00:31:45 -0800

    add faker

--- uberforbooks/db/seeds.rb
+++ uberforbooks/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do |index|
+  User.create!(first_name: Faker::Name.first_name,
+                    last_name: Faker::Name.last_name,
+                    bio: Faker::HarryPotter.quote,
+                    email: Faker::Internet.email,
+                    encrypted_password: Faker::Internet.password
+                    )
+end
